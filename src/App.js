e7ed75efaf4b7d24156f6e8f127ed5bbc13e0a78
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-19 10:44:31 -0600

    Add files via upload
    color generator - structure
    in App.js added variables color, error, list, and handleSubmit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  return <h1>yes sir</h1>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+  return (
+    <>
+      <section className="container">yes sir</section>;
+    </>
+  );
 }
 
 export default App;
