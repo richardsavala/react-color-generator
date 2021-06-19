298ea1c8c50c4276ca17b7adf9da5e9b7880b2f0
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-18 23:46:42 -0600

    Add files via upload
    color generator
    color generator-intro
    initial commit
    in package.json updated 'react-scripts' to latest version 4.0.3

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  return <h1>yes sir</h1>;
+}
+
+export default App;
