153ee172d3ac0ff6fb3ae8c5a3897bfb2a82df84
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-21 05:46:05 -0600

    Add files via upload
    color generator - single color
    in App.js added list.map to second section 'colors', added return SingleColor

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
+      setError(true);
       console.log(error);
     }
   };
@@ -33,9 +34,12 @@ function App() {
           </button>
         </form>
       </section>
-      ;
+
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
